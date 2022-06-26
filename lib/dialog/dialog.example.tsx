ab951bc32755d08e427c0e4b2331c8580f3eb20b
ViktorVan22 <zcg5368@163.com>
2022-06-27 02:52:56 +0800

    完成dialog组件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,13 +3,77 @@ import { Dialog } from "./dialog";
 
 const DialogExample = () => {
   const [x, setX] = useState(false);
-  const openDialog = () => {
-    setX(!x);
-  };
+  const [y, setY] = useState(false);
   return (
     <div>
-      <button onClick={openDialog}>点击出现对话框</button>
-      <Dialog visible={x} />
+      <div
+        style={{
+          position: "relative",
+          zIndex: 10,
+          border: "1px solid red",
+          color: "red",
+        }}
+      >
+        <h1>example 1</h1>
+        <button onClick={() => setX(!x)}>click</button>
+        <Dialog
+          visible={x}
+          buttons={[
+            <button
+              onClick={() => {
+                setX(false);
+              }}
+            >
+              1
+            </button>,
+            <button
+              onClick={() => {
+                setX(false);
+              }}
+            >
+              2
+            </button>,
+          ]}
+          onClose={() => {
+            setX(false);
+          }}
+        >
+          <strong>hi</strong>
+        </Dialog>
+      </div>
+      <div style={{ position: "relative", zIndex: 9 }}>
+        <h1>example 2</h1>
+        <button onClick={() => setY(!y)}>click</button>
+        <Dialog
+          visible={y}
+          closeOnClickMask={true}
+          buttons={[
+            <button
+              onClick={() => {
+                setY(false);
+              }}
+            >
+              1
+            </button>,
+            <button
+              onClick={() => {
+                setY(false);
+              }}
+            >
+              2
+            </button>,
+          ]}
+          onClose={() => {
+            setY(false);
+          }}
+        >
+          <strong>hi</strong>
+        </Dialog>
+      </div>
+      <div>
+        <h1>example 3</h1>
+        <button onClick={() => alert("1")}>alert</button>
+      </div>
     </div>
   );
 };
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { Fragment, ReactElement } from "react";
+import ReactDOM from "react-dom";
+import "./dialog.scss";
 
-interface DialogProps {
+interface Props {
   visible: boolean;
+  buttons?: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
+  closeOnClickMask?: Boolean;
+  children: React.ReactNode;
 }
 
-const Dialog: React.FC<DialogProps> = props => {
-  return props.visible ? <div>I am dialog</div> : null;
+const Dialog: React.FunctionComponent<Props> = props => {
+  // const onClickClose: React.MouseEventHandler = e => {
+  //   props.onClose(e);
+  // };
+
+  // const onClickMask: React.MouseEventHandler = e => {
+  //   if (props.closeOnClickMask) {
+  //     props.onClose(e);
+  //   }
+  // };
+
+  const x = props.visible ? (
+    <Fragment>
+      <div className="steveUI-dialog-mask"></div>
+      <div className="steveUI-dialog">
+        <div className="steveUI-dialog-close">close</div>
+        <header className="steveUI-dialog-header">提示</header>
+        <main className="steveUI-dialog-main">{props.children}</main>
+        <footer className="steveUI-dialog-footer">
+          {props.buttons &&
+            props.buttons.map((button, index) =>
+              React.cloneElement(button, { key: index })
+            )}
+        </footer>
+      </div>
+    </Fragment>
+  ) : null;
+  return ReactDOM.createPortal(x, document.body);
 };
 
+Dialog.defaultProps = {
+  closeOnClickMask: false,
+};
+
+const alert = (content: string) => {
+  const component = (
+    <Dialog
+      visible={true}
+      onClose={() => {
+        ReactDOM.render(React.cloneElement(component, { visible: false }), div);
+        ReactDOM.unmountComponentAtNode(div);
+        div.remove();
+      }}
+    >
+      {content}
+    </Dialog>
+  );
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(component, div);
+};
+
+export { alert };
 export { Dialog };
